d38eae2fa7c469cee97d62856ea140abee71e762
jeanfern5 <jeanfern555@gmail.com>
2024-04-28 09:32:38 -0700

    Chapter 3 Lecture 2: Thread Creation

--- chapter_3/lecture_2-Thread-Priorities-and-States/src/main/java/Main.java
+++ chapter_3/lecture_2-Thread-Priorities-and-States/src/main/java/Main.java
@@ -1,22 +1,27 @@
-public class Main {
+// Chapter 3 Lecture 2: Thread Priority and States
+
+import javax.swing.plaf.TableHeaderUI;
+
+class Scratch {
 //    public static void main(String[] args) throws InterruptedException {
+//        // main thread
 //        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
 //
+//        // new thread
 //        Thread thread1 = new Thread(() -> {
 //            Thread currentThread = Thread.currentThread();
-//            System.out.println(currentThread.getName() + " priority = " + currentThread.getPriority());
+//            System.out.println(currentThread.getName() + ": priority = " + currentThread.getPriority());
 //        });
-//
 //        thread1.setName("Thread_1");
 //        thread1.setPriority(Thread.MAX_PRIORITY);
 //
 //        Thread thread2 = new Thread(() -> {
 //            Thread currentThread = Thread.currentThread();
-//            System.out.println(currentThread.getName() + " priority = " + currentThread.getPriority());
+//            System.out.println(currentThread.getName() + ": priority = " + currentThread.getPriority());
 //        });
-//
 //        thread2.setName("Thread_2");
 //        thread2.setPriority(Thread.MIN_PRIORITY);
+//
 //
 //        thread1.start();
 //        thread2.start();
@@ -26,19 +31,18 @@ public class Main {
 //    }
 
     public static void main(String[] args) throws InterruptedException {
+        // new thread
         Thread thread = new Thread(() -> {
             Thread currentThread = Thread.currentThread();
-            System.out.println("[1] State: " + currentThread.getState());
+            System.out.println("[1] State: " + currentThread.getState()); // RUNNABLE
         });
 
-        System.out.println("[2] State: " + thread.getState());
+        System.out.println("[2] State: " + thread.getState()); // NEW
 
-        thread.start();
+        thread.start(); // started new thread
+        System.out.println("[3] State: " + thread.getState()); // RUNNABLE
 
-        System.out.println("[3] State: " + thread.getState());
-
-        thread.join();
-
-        System.out.println("[4] State: " + thread.getState());
+        thread.join(); // joins main thread
+        System.out.println("[4] State: " + thread.getState()); // TERMINATE
     }
 }
