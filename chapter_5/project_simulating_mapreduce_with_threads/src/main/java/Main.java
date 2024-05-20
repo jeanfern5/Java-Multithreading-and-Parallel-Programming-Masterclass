8e3abac3439468513d623b470eed1b5eab1dc5db
jeanfern5 <jeanfern555@gmail.com>
2024-05-19 18:02:44 -0700

    Completed project

--- chapter_5/project_simulating_mapreduce_with_threads/src/main/java/Main.java
+++ chapter_5/project_simulating_mapreduce_with_threads/src/main/java/Main.java
@@ -2,8 +2,10 @@ import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
 
+/**
+ * Simulating MapReduce with Threads
+ */
 public class Main {
-
     /*
 
     -> Intermediate result:
@@ -30,36 +32,35 @@ public class Main {
         ],
         ...
     ]
-
-
-
      */
-
     private static final String input = "a friend in need is a friend indeed";
-
-    private static final CountDownLatch countDownLatch = new CountDownLatch(2);
-
+    private static final CountDownLatch countDownLatch = new CountDownLatch(2); // 2 counts == 2 Mapper threads
     private static final List<Map.Entry<String, Integer>> intermediateResult = Collections.synchronizedList(new ArrayList<>());
     private static final List<List<Map.Entry<String, Integer>>> reducersInput = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) throws InterruptedException {
         List<String> inputList = Arrays.asList(input.split(" "));
 
-        new Thread(new Mapper(inputList.subList(0, inputList.size() / 2))).start();
-        new Thread(new Mapper(inputList.subList(inputList.size() / 2, inputList.size()))).start();
+        // Map - map words in parallel between two threads and create "inputReducer"
+        int midListIdx = inputList.size() / 2;
+        new Thread(new Mapper(inputList.subList(0, midListIdx))).start();
+        new Thread(new Mapper(inputList.subList(midListIdx, inputList.size()))).start();
 
+        // Partition - filer unique words from inputReducer and re-order words group same words together
         Thread partitioner = new Thread(new Partitioner());
         partitioner.start();
-        partitioner.join();
+        partitioner.join(); // makes sure the partitioner is complete before going to the Reducer - waits for the thread to die
 
-        for (List<Map.Entry<String, Integer>> reducerInput : reducersInput) {
+        // Reducer - count words in each group and print count of each word
+        for (List<Map.Entry<String,Integer>> reducerInput: reducersInput) {
             new Thread(new Reducer(reducerInput)).start();
         }
+
+//        System.out.println("Test"); // useful breakpoint to see values from `intermediateResult` and `reducersInput`
     }
 
     static class Mapper implements Runnable {
         private final List<String> input;
-
         public Mapper(List<String> input) {
             this.input = input;
         }
@@ -69,18 +70,19 @@ public class Main {
             for (String word : input) {
                 intermediateResult.add(Map.entry(word, 1));
             }
+            // guarantees that `Partitioner` thread won't start before `Mapper` threads are done
             countDownLatch.countDown();
         }
     }
 
     static class Partitioner implements Runnable {
-
         @Override
         public void run() {
             try {
+                // guarantees that `Partitioner` thread won't start before `Mapper` threads are done
                 countDownLatch.await();
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
 
             List<String> uniqueWords = intermediateResult.stream()
@@ -98,21 +100,19 @@ public class Main {
     }
 
     static class Reducer implements Runnable {
-
         private final List<Map.Entry<String, Integer>> reducerInput;
-
         public Reducer(List<Map.Entry<String, Integer>> reducerInput) {
             this.reducerInput = reducerInput;
         }
 
         @Override
         public void run() {
-            int S = 0;
-            for (Map.Entry<String, Integer> entry: reducerInput) {
-                S += entry.getValue();
+            int sum = 0;
+            for (Map.Entry<String, Integer> entry : reducerInput) {
+                sum += entry.getValue();
             }
 
-            System.out.println("The word: " + reducerInput.get(0).getKey() + " -> occurences: " + S);
+            System.out.println("The word: " + reducerInput.get(0).getKey() + " -> occurrences: " + sum);
         }
     }
 }
