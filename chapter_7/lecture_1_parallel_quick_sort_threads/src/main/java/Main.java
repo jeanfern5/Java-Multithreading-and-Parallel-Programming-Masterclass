862583d6ba2825c44cb8cb3ec9443166f4dfc6b3
jeanfern5 <jeanfern555@gmail.com>
2024-08-11 17:43:02 -0700

    Completed Ch7 - Lecture 1 - Bringing Parallelism to Quick Sort

--- chapter_7/lecture_1_parallel_quick_sort_threads/src/main/java/Main.java
+++ chapter_7/lecture_1_parallel_quick_sort_threads/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveAction;
 
 public class Main {
@@ -8,20 +10,6 @@ public class Main {
     private static int arraySize = 100000000;
     private static int[] array = new int[arraySize];
 
-    // Fork Join
-    // Run 1 -> 5.814 seconds
-    // Run 2 -> 5.504 seconds
-    // Run 3 -> 5.758 seconds
-    // Run 4 -> 6.038 seconds
-    // Run 5 -> 5.495 seconds
-
-    // Classical recursive
-    // Run 1 -> 10.87 seconds
-    // Run 2 -> 10.94 seconds
-    // Run 3 -> 10.77 seconds
-    // Run 4 -> 10.83 seconds
-    // Run 5 -> 10.80 seconds
-
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         for (int i = 0; i < arraySize; i++) {
             array[i] = random.nextInt(arraySize);
@@ -29,28 +17,33 @@ public class Main {
 
 //        System.out.println(Arrays.toString(array));
 
-
-
         long start = System.nanoTime();
 
         quickSort(0, arraySize - 1);
+        // Classical recursive execution
+        // Run 1 -> 13.223 seconds
+        // Run 2 -> 12.260 seconds
+        // Run 3 -> 12.364 seconds
+        // Run 4 -> 14.279 seconds
+        // Run 5 -> 12.459 seconds
 
 //        ForkJoinPool forkJoinPool = new ForkJoinPool();
 //        ForkJoinTask<Void> future = forkJoinPool.submit(new QuickSortTask(0, arraySize - 1));
-//
 //        future.get();
+        // Fork Join execution
+        // Run 1 -> 3.151 seconds
+        // Run 2 -> 2.857 seconds
+        // Run 3 -> 2.858 seconds
+        // Run 4 -> 2.768 seconds
+        // Run 5 -> 3.509 seconds
 
         long end = System.nanoTime();
 
         System.out.println("Execution time = " + (end - start));
-
-
 //        System.out.println(Arrays.toString(array));
-
     }
 
     static class QuickSortTask extends RecursiveAction {
-
         private final int left;
         private final int right;
 
@@ -63,27 +56,17 @@ public class Main {
         protected void compute() {
             if (left < right) {
                 int pivotIndex = partition(left, right);
-                invokeAll(new QuickSortTask(left, pivotIndex - 1),
-                        new QuickSortTask(pivotIndex + 1, right));
-            }
-        }
-    }
 
-    static int partition(int left, int right) {
-        int pivot = array[right];
-        int swapIndex = left - 1;
+                // Fork Join
+//                invokeAll(new QuickSortTask(left, pivotIndex - 1),
+//                        new QuickSortTask(pivotIndex + 1, right));
 
-        for (int i = left; i < right; i++) {
-            if(array[i] < pivot) {
-                swapIndex++;
-                swap(swapIndex, i);
+                // Classical recursive
+                quickSort(left, pivotIndex - 1);
+                quickSort(pivotIndex + 1, right);
             }
         }
 
-        swapIndex++;
-        swap(swapIndex, right);
-
-        return swapIndex;
     }
 
     static void quickSort(int left, int right) {
@@ -95,9 +78,26 @@ public class Main {
         }
     }
 
-    static void swap(int leftIndex, int rightIndex) {
-        int aux = array[leftIndex];
-        array[leftIndex] = array[rightIndex];
-        array[rightIndex] = aux;
+    static int partition(int left, int right) {
+        int pivot = array[right];
+        int swapIndex = left - 1;
+
+        for (int i = left; i < right; i ++) {
+            if (array[i] < pivot) {
+                swapIndex++;
+                swap(swapIndex, i);
+            }
+        }
+
+        swapIndex++;
+        swap(swapIndex, right);
+        return swapIndex;
     }
+
+    static void swap(int leftIndex, int rightIndex) {
+        int temp = array[leftIndex];
+        array[leftIndex] = array[rightIndex];
+        array[rightIndex] = temp;
+    }
+
 }
